76624587e204c450bef5ac2ebbe15a09a144afe5
Martin <sq.martin91@gmail.com>
2016-02-29 09:11:52 +0200

    person klass

---
+++ golf/golf/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace golf.Models
+{
+    public class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string streetName {get;set;}
+        public string postalCode {get;set;}
+        public string city { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+        public double HCP { get; set; }
+        public int golfID { get; set; }
+        public string memberType { get; set; }
+        public string PW { get; set; }
+        
+    }
+}
