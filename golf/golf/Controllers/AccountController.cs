f7e3c172b2117032daf7b6c96a5565742f2b6af2
Michael Byrskog <micke884@hotmail.com>
2016-03-01 15:40:28 +0100

    ändringar login

--- golf/golf/Controllers/AccountController.cs
+++ golf/golf/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using golf.Models;
 
 namespace golf.Controllers
 {
@@ -11,7 +12,7 @@ namespace golf.Controllers
     {
         //
         // GET: /Account/
-        
+        private dsuteam4Entities1 db = new dsuteam4Entities1();
         public ActionResult Egister()
         {
             ViewBag.Message = "Egister ig är ör att li edlem";
@@ -31,6 +32,21 @@ namespace golf.Controllers
         {
             FormsAuthentication.SignOut();
             return View();
+        }
+        public ActionResult LogInCheck(Person model)
+        {
+
+            String email = model.email;
+            String PW = model.PW;
+            foreach (Person P in db.Person)
+            {
+                if (P.email == email && P.PW == PW)
+                {
+                    FormsAuthentication.SetAuthCookie(model.email, true);
+                    return View("~/Views/Home/Index.cshtml");
+                }
+            }
+            return View("~/Views/Log/Index.cshtml");
         }
     }
 }
