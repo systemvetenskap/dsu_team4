24420d801133f13d80266f4e2cda85e294a27e26
Michael Byrskog <micke884@hotmail.com>
2016-03-03 14:27:12 +0100

    Börjat på nyheter.

---
+++ golf/golf/Controllers/NewsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using golf.Models;
+
+namespace golf.Controllers
+{
+    public class NewsController : Controller
+    {
+        //
+        // GET: /News/
+        private dsuteam4Entities1 db = new dsuteam4Entities1();
+        public ActionResult Index()
+        {
+            NewsClass na = new NewsClass();
+
+            foreach (NewsArticle article in db.NewsArticle)
+            {
+                na.newarticle.Add(article);
+            }
+            return View(na);
+        }
+
+        public ActionResult AddNews()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ golf/golf/Models/NewsClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace golf.Models
+{
+    public class NewsClass
+    {
+        public List<NewsArticle> newarticle = new List<NewsArticle>();
+
+    }
+}
