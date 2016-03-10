e0f7ec67e672a8446cef18b5a0fcc3c10ca5cd24
hnkrpnk <hkauhanen@live.se>
2016-03-10 15:28:20 +0100

    cc creatComp

--- golf/golf/Controllers/CompetitionController.cs
+++ golf/golf/Controllers/CompetitionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using golf.Models;
 
 namespace golf.Controllers
 {
@@ -13,8 +14,11 @@ namespace golf.Controllers
 
         public ActionResult Index()
         {
+           CreateComp cc = new CreateComp();
 
-            return View();
+           cc.currentDate = DateTime.Today;
+
+            return View(cc);
         }
         public void getData()
         {
