4e3a026a9e855d35d08db86f19bba8452d4c7a33
mNesterud <mnesterud@gmail.com>
2016-03-11 00:15:40 +0100

    alla värden för skapa tävling kan nu fyllas i direkt i gränssnittet. Funktion för att Skapa tävling fullt fungerande

--- golf/golf/Controllers/CompetitionController.cs
+++ golf/golf/Controllers/CompetitionController.cs
@@ -20,8 +20,30 @@ namespace golf.Controllers
            cc.currentDate = DateTime.Today;
             using (dsuteam4Entities1 databas = new dsuteam4Entities1())
             {
+
                 
+
                 cc.classList = databas.CompeteClass.ToList();
+                List<Person> p = databas.Person.ToList();
+                List<OneNamePerson> op = new List<OneNamePerson>();
+                foreach (Person i in p)
+                {
+                    OneNamePerson onp = new OneNamePerson();
+                    onp.Id            =  i.Id;           
+                    onp.oneName       =  i.firstName + " " + i.lastName;      
+                    onp.firstName     =  i.firstName;    
+                    onp.lastName      =  i.lastName;     
+                    onp.streetAddres  =  i.streetAddres ;
+                    onp.postalCode    =  i.postalCode;  
+                    onp.city          =  i.city;
+                    onp.email         =  i.email;  
+                    onp.gender_ID     =  i.gender_ID;
+                    onp.memberType_ID =  i.memberType_ID;
+                    op.Add(onp);
+                }
+                cc.contactlist = op.OrderBy(x => x.oneName);
+                
+
                 //cc.contactlist = databas.Person.ToList();
 
             }
@@ -36,8 +58,8 @@ namespace golf.Controllers
                 var comp = cc.newComp;
 
                 //HÄR HÅRDKODAS DET------->
-                comp.Person_IDc = 25;
-                comp.CompeteClass_ID = 1;
+                //comp.Person_IDc = 25;
+                //comp.CompeteClass_ID = 1;
                 //HÄR HÅRDKODAS DET-------<
             
                 databas.Competition.Add(comp); 
