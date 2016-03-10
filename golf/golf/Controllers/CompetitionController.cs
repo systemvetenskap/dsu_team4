c3ead397316d12211de72158a1a3b6ad4a7ae751
hnkrpnk <hkauhanen@live.se>
2016-03-10 13:17:23 +0100

    cc showComp

---
+++ golf/golf/Controllers/CompetitionController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace golf.Controllers
+{
+    public class CompetitionController : Controller
+    {
+        //
+        // GET: /Competition/
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public void getData()
+        {
+
+        }
+
+    }
+}
