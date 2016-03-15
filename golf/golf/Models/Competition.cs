dfeda6cc92f69db7433815d7d846e3fb3185e1b0
hnkrpnk <hkauhanen@live.se>
2016-03-15 13:32:50 +0100

    cc fixJSonIGnore

--- golf/golf/Models/Competition.cs
+++ golf/golf/Models/Competition.cs
@@ -11,7 +11,7 @@ namespace golf.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using Newtonsoft.Json;
     public partial class Competition
     {
         public Competition()
@@ -31,9 +31,12 @@ namespace golf.Models
         public int NumberOfHoles { get; set; }
         public string startTime { get; set; }
         public string endTime { get; set; }
-    
+        
+        [JsonIgnore]
         public virtual CompeteClass CompeteClass { get; set; }
+        [JsonIgnore]
         public virtual Person Person { get; set; }
+        [JsonIgnore]
         public virtual ICollection<CompetitionGolfer> CompetitionGolfer { get; set; }
     }
 }
